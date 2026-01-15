41a1ec2b4f68e2064e923f27f0bb95b2359a6035
David Castano <david.castano@toyota.com>
2026-01-15 13:45:13 +0100

    added placeholder

---
+++ Sources/FlutterIntegration/Placeholder.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+// This file exists to satisfy Swift Package Manager.
+// It allows the 'FlutterIntegration' target to build and 
+// expose the binary dependencies (App & Flutter).
+public struct FlutterIntegrationStub {
+    public init() {}
+}
