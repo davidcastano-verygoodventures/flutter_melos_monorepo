fe21e9649ff1afd6fde06622ba49ff67d0c10cec
GitHub Action <action@github.com>
2026-01-15 10:35:26 +0000

    Update Package.swift for

--- Package.swift
+++ Package.swift
@@ -22,13 +22,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "App",
-            url: "https://github.com/myuser/myrepo/releases/download/v1.0.0/App.xcframework.zip",
-            checksum: "c3956bf81a3f0da62a4cc56a66ae8aebaddfdfbef030b3807ae3d9c0bc56566d"
+            url: "https://github.com/davidcastano-verygoodventures/flutter_melos_monorepo/releases/download/vmain/App.xcframework.zip",
+            checksum: "e9a5a596f771857ddd8765f3376692aabaa012b5588e5c564299a27eb519c02f"
         ),
         .binaryTarget(
             name: "Flutter",
-            url: "https://github.com/myuser/myrepo/releases/download/v1.0.0/Flutter.xcframework.zip",
-            checksum: "ca83a2ccf2fc7f309cd9b08fec9ab0bb959ca0b30134fbac33b6010ff29862a8"
+            url: "https://github.com/davidcastano-verygoodventures/flutter_melos_monorepo/releases/download/vmain/Flutter.xcframework.zip",
+            checksum: "2d9847d8b772a916f2f16ce0f353e49b283767ffb1ed55fb948b695461282562"
         )
     ]
 )
