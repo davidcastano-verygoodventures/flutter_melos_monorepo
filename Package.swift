cfdb22e1e5313014f3e0e5fa76fa727a9f164322
GitHub Action <action@github.com>
2026-01-15 13:34:02 +0000

    Update Package.swift for

--- Package.swift
+++ Package.swift
@@ -19,24 +19,34 @@ let package = Package(
             name: "Flutter",
             targets: ["Flutter"]
         ),
+        .library(
+            name: "FlutterPluginRegistrant",
+            targets: ["FlutterPluginRegistrant"]
+        ),
     ],
     targets: [
         .target(
             name: "FlutterIntegration",
             dependencies: [
                 .target(name: "App"),
-                .target(name: "Flutter")
+                .target(name: "Flutter"),
+                .target(name: "FlutterPluginRegistrant"),
             ]
         ),
         .binaryTarget(
             name: "App",
-            url: "https://github.com/davidcastano-verygoodventures/flutter_melos_monorepo/releases/download/1.1.4/App.xcframework.zip",
-            checksum: "302b2c0baa4c31e0b4eddcb27c20c2ccc55f2875d1efaa9ab75f1e2175549c71"
+            url: "https://github.com/davidcastano-verygoodventures/flutter_melos_monorepo/releases/download/1.1.6/App.xcframework.zip",
+            checksum: "14f954afb9f8d05aa2a07a423542751da39179fd173f0c189274f1a858acc6e2"
         ),
         .binaryTarget(
             name: "Flutter",
-            url: "https://github.com/davidcastano-verygoodventures/flutter_melos_monorepo/releases/download/1.1.4/Flutter.xcframework.zip",
-            checksum: "fe7642db29c1bed9b951365a2cc257d4357d500020643f1bd072a4da5ab47c9d"
-        )
+            url: "https://github.com/davidcastano-verygoodventures/flutter_melos_monorepo/releases/download/1.1.6/Flutter.xcframework.zip",
+            checksum: "71d33deb44d817545b2629dad460dbfda06b2043d44df40c9c36ebe9abd175a2"
+        ),
+        .binaryTarget(
+            name: "FlutterPluginRegistrant",
+            url: "https://github.com/davidcastano-verygoodventures/flutter_melos_monorepo/releases/download/1.1.6/FlutterPluginRegistrant.xcframework.zip",
+            checksum: "c0603dcd624b09ff8b0103a74eb8d79ded95c837fb4ea0f7be469d7b0299d33d"
+        ),
     ]
 )
