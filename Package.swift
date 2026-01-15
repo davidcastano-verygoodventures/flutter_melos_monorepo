cba2c2a25cb2d4b08831c874d8f2675ff4d5424b
David Castano <david.castano@toyota.com>
2026-01-15 13:06:18 +0100

    fix: update Package.swift with correct release URLs

--- Package.swift
+++ Package.swift
@@ -30,13 +30,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "App",
-            url: "https://github.com/davidcastano-verygoodventures/flutter_melos_monorepo/releases/download/vcore_utils-v1.1.0/App.xcframework.zip",
-            checksum: "2ba2b5b02d2f81b838ffd927f91f90913d1557fa6f3b4563a2b68f2fc53cb144"
+            url: "https://github.com/davidcastano-verygoodventures/flutter_melos_monorepo/releases/download/core_utils-v1.1.0/App.xcframework.zip",
+            checksum: "c3956bf81a3f0da62a4cc56a66ae8aebaddfdfbef030b3807ae3d9c0bc56566d"
         ),
         .binaryTarget(
             name: "Flutter",
-            url: "https://github.com/davidcastano-verygoodventures/flutter_melos_monorepo/releases/download/vcore_utils-v1.1.0/Flutter.xcframework.zip",
-            checksum: "6af6e24cd1fdd7e41923d4f6cbf47e14046cc7bad313f663a72053479869d39d"
+            url: "https://github.com/davidcastano-verygoodventures/flutter_melos_monorepo/releases/download/core_utils-v1.1.0/Flutter.xcframework.zip",
+            checksum: "ca83a2ccf2fc7f309cd9b08fec9ab0bb959ca0b30134fbac33b6010ff29862a8"
         )
     ]
 )
